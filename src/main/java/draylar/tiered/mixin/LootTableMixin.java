853ef2c6841c986a525a1561afb5def8739bc91c
Globox1997 <mitschke11@web.de>
2022-05-02 21:59:54 +0200

    added loot tiering

---
+++ src/main/java/draylar/tiered/mixin/LootTableMixin.java
@@ -0,0 +1,27 @@
+package draylar.tiered.mixin;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import draylar.tiered.api.ModifierUtils;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContext;
+
+@Mixin(LootTable.class)
+public class LootTableMixin {
+
+    @Inject(method = "supplyInventory", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/Inventory;setStack(ILnet/minecraft/item/ItemStack;)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void supplyInventoryMixin(Inventory inventory, LootContext context, CallbackInfo info, List<ItemStack> list, Random random, List<Integer> list2, Iterator<ItemStack> var6,
+            ItemStack itemStack) {
+        ModifierUtils.setItemStackAttribute(itemStack);
+    }
+}
